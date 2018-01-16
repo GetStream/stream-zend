01a42ec6fece394814c15a0d6a3624a6a8b99043
Hannes Van De Vreken <vandevreken.hannes@gmail.com>
2018-01-16 15:43:42 +0100

    Full test suite

--- tests/unit/ModuleTest.php
+++ tests/unit/ModuleTest.php
@@ -2,7 +2,11 @@
 
 namespace GetStream\Zend\Unit;
 
+use GetStream\Stream\Client;
+use GetStream\Zend\Module;
 use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+use Zend\ModuleManager\Feature\ServiceProviderInterface;
 
 class ModuleTest extends TestCase
 {
@@ -10,31 +14,85 @@ class ModuleTest extends TestCase
     public function implementsServiceProviderInterface()
     {
         // Arrange
+        $module = new Module();
+
         // Act
         // Assert
+        $this->assertInstanceOf(ServiceProviderInterface::class, $module);
     }
 
     /** @test */
     public function setsClientFactory()
     {
         // Arrange
+        $module = new Module();
+
         // Act
+        $config = $module->getServiceConfig();
+
         // Assert
+        $this->assertContains('factories', array_keys($config));
+        $this->assertContains(Client::class, array_keys($config['factories']));
+        $this->assertInternalType('callable', $config['factories'][Client::class]);
     }
 
     /** @test */
     public function sharesClient()
     {
         // Arrange
+        $module = new Module();
+
         // Act
+        $config = $module->getServiceConfig();
+
         // Assert
+        $this->assertContains('shared', array_keys($config));
+        $this->assertContains(Client::class, array_keys($config['shared']));
+        $this->assertTrue($config['shared'][Client::class]);
     }
 
     /** @test */
-    public function factoryInitiatesClientFromConfig()
+    public function factoryInitiatesClientFromURL()
     {
         // Arrange
+        $module = new Module();
+        $container = $this->createMock(ContainerInterface::class);
+        $container->expects($this->once())->method('get')->with('config')->willReturn([
+            'stream' => [
+                'url' => 'https://blabla:blabla@api.stream-api.com/',
+            ],
+        ]);
+
         // Act
+        $config = $module->getServiceConfig();
+        $callable = $config['factories'][Client::class];
+
         // Assert
+        $response = $callable($container);
+
+        $this->assertInstanceOf(Client::class, $response);
+    }
+
+    /** @test */
+    public function factoryInitiatesClientFromCredentials()
+    {
+        // Arrange
+        $module = new Module();
+        $container = $this->createMock(ContainerInterface::class);
+        $container->expects($this->once())->method('get')->with('config')->willReturn([
+            'stream' => [
+                'app_key' => 'mock key',
+                'app_secret' => 'mock secret',
+            ],
+        ]);
+
+        // Act
+        $config = $module->getServiceConfig();
+        $callable = $config['factories'][Client::class];
+
+        // Assert
+        $response = $callable($container);
+
+        $this->assertInstanceOf(Client::class, $response);
     }
 }
